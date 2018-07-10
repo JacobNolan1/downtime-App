4a34a66e5c34d5ec68bfebcc805c5268b4397ca1
JacobNolan1 <jacobnolan1998@gmail.com>
2018-07-10 22:24:58 +1000

    Javascript for selecting activity from dropdowns

--- downtime_App/static/js/javascript.js
+++ downtime_App/static/js/javascript.js
@@ -2,4 +2,108 @@ $(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });
+
+    $('#activities-category').change(selectActivities);
 });
+
+function selectActivities() {
+
+	var category = ($('#activities-category').val());
+	$('#activities-list').children().remove();
+	if(category === "General Activities") {
+		selectGeneral();
+	} else if(category === "Job") {
+		selectJob();
+	} else if(category === "Town") {
+		selectTown();
+	} else if(category === "City") {
+		selectCity();
+	} else if(category === "Wilderness") {
+		selectWilderness();
+	} else if(category === "Dungeon") {
+		selectDungeon();
+	} else if(category === "Other") {
+		selectOther();
+	} else {
+		alert("Not Selected");
+	}
+}
+
+function selectGeneral() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : 'Craft an Item',
+    val3 : 'Buy an Item',
+    val4 : 'Sell an Item'
+	};
+	setSelectElements(activities);
+}
+
+function selectJob() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function selectTown() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function selectCity() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function selectWilderness() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function selectDungeon() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function selectOther() {
+	var activities = {
+    val1 : '--Select--',
+    val2 : '---',
+    val3 : '---',
+    val4 : '---'
+	};
+	setSelectElements(activities);
+}
+
+function setSelectElements(activities) {
+	$.each(activities, function(key, value) {
+     $('#activities-list')
+         .append($("<option></option>")
+         .attr("value",key)
+         .text(value));
+     });
+}
